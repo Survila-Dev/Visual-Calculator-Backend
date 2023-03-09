cae7a697a92238509f0b93c1f746de86e8be775d
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-03-09 18:34:05 +0100

    create server boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 import express from "express"
+import { Express } from "express"
 
+const app: Express = express()
+const port: string | undefined = process.env.PORT
+
+app.use(express.json())
+
+app.listen(() => console.log(`Server is listening to port ${port}`))
